d5957b68c511ca4bc6fb0eb129ce9dff9c27a231
saffytaylor <snft1@st-andrews.ac.uk>
2024-03-31 02:41:21 +0100

    update sat night

--- converting to CSV.R
+++ converting to CSV.R
@@ -2,8 +2,8 @@
 
 rm(list = ls()) # emptying the environment
 
-install.packages('haven') # package that allows us to read .sav files
-install.packages('readr') # package that allows us to save the data as a .csv
+install.packages('haven') # package that allows us to read .sav files (Haven, 2024)
+install.packages('readr') # package that allows us to save the data as a .csv (Readr, 2024)
 
 library(haven) 
 library(readr) 
@@ -14,8 +14,3 @@ getwd()
 
 BSA_2020 <- read_sav("bsa2020_archive.sav")
 write_csv(x=BSA_2020, path="bsa2020.csv")
-
-# https://haven.tidyverse.org/reference/read_spss.html
-# https://readr.tidyverse.org/reference/write_delim.html
-
-# https://forum.posit.co/t/how-do-i-convert-a-sav-file-to-a-csv-file-in-r/73366
